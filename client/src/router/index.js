7795fb6263101f757e0cae7838e43ff25c400d0f
dilapitan <domlapitan@gmail.com>
2022-08-28 17:35:38 +0800

    Refactor routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import BeneficiariesView from '../views/BeneficiariesView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'BeneficiariesView',
+    component: BeneficiariesView,
+  },
   {
     path: '/about',
     name: 'AboutView',
@@ -21,15 +27,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/AddBeneficiaryView.vue'),
-  },
-  {
-    path: '/',
-    name: 'BeneficiariesView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/BeneficiariesView.vue'),
   },
   {
     path: '*',

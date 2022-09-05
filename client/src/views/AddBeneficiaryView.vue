fecb2b66f8018f1fb77807f31f8df9a8f351f35e
dilapitan <domlapitan@gmail.com>
2022-09-05 14:45:15 +0800

    Update UI of Add Beneficiary

--- client/src/views/AddBeneficiaryView.vue
+++ client/src/views/AddBeneficiaryView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an add BENEFICIARIES page</h1>
+  <div class="my-5 mx-10">
+    <h3>Add Beneficiary</h3>
+    <p class="mt-2 text-body-2">
+      This is where you can add a new beneficiary to the list.
+    </p>
+    <br />
+    <v-card color="middleground" flat height="100%">
+      <p>This is the form</p>
+    </v-card>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AddBeneficiaryView',
+}
+</script>

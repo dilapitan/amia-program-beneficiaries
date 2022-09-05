6e08669d460b562418861d068657ec12404257ea
dilapitan <domlapitan@gmail.com>
2022-09-05 20:34:25 +0800

    Add Part 1 in Add B

--- client/src/views/AddBeneficiaryView.vue
+++ client/src/views/AddBeneficiaryView.vue
@@ -6,6 +6,7 @@
     </p>
     <br />
     <v-card class="pa-2" color="middleground" flat height="100%">
+      <!-- 0 General -->
       <v-row>
         <v-col class="d-flex align-baseline" cols="12" sm="6" md="3">
           <div class="mr-2">
@@ -106,6 +107,67 @@
           </div>
         </v-col>
       </v-row>
+
+      <br />
+      <v-divider></v-divider>
+      <br />
+
+      <!-- 1 Farmer's Basic Information -->
+
+      <h3>I. FARMER'S BASIC INFORMATION</h3>
+
+      <br />
+
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.1) Region/Province:</div>
+
+        <div>
+          <v-text-field
+            dense
+            clearable
+            v-model="regionOrProvince"
+          ></v-text-field>
+        </div>
+      </div>
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.2) City/Municipality:</div>
+
+        <div>
+          <v-text-field
+            dense
+            clearable
+            v-model="cityOrMunicipality"
+          ></v-text-field>
+        </div>
+      </div>
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.3) Barangay:</div>
+
+        <div>
+          <v-text-field dense clearable v-model="barangay"></v-text-field>
+        </div>
+      </div>
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.4) Name of Farmer:</div>
+
+        <div>
+          <v-text-field dense clearable v-model="nameOfFarmer"></v-text-field>
+        </div>
+      </div>
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.5) Contact No:</div>
+
+        <div>
+          <v-text-field dense clearable v-model="contactNo"></v-text-field>
+        </div>
+      </div>
+      <div class="d-flex align-baseline" cols="12" sm="6" md="6">
+        <div class="mr-2 text-body-2">(1.6) Farmer's Code No:</div>
+
+        <div>
+          <v-text-field dense clearable v-model="farmersCodeNo"></v-text-field>
+        </div>
+      </div>
     </v-card>
   </div>
 </template>
@@ -124,6 +186,14 @@ export default {
 
     // 0. General Info - UI variables
     dateMenu: false,
+
+    // 1. Farmer's Basic Information
+    regionOrProvince: null,
+    cityOrMunicipality: null,
+    barangay: null,
+    nameOfFarmer: null,
+    contactNo: null,
+    farmersCodeNo: null,
   }),
 }
 </script>

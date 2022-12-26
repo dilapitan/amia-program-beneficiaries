42050a31d0f6cc71b4d44765643adca91b6b223f
dilapitan <domlapitan@gmail.com>
2022-12-27 07:32:57 +0800

    Add initial preview for Beneficiary Form

--- client/src/views/BeneficiaryFormView.vue
+++ client/src/views/BeneficiaryFormView.vue
@@ -137,10 +137,10 @@
             v-if="mode !== 'VIEW'"
             :disabled="loading"
             :loading="loading"
-            @click="
+            @click.stop="
               mode === 'EDIT'
                 ? updateBeneficiary(currentBeneficiary)
-                : addBeneficiary()
+                : confirmAddOrUpdateBeneficiary()
             "
             color="primary"
           >
@@ -151,6 +151,30 @@
           </v-btn>
         </div>
       </v-form>
+
+      <v-dialog v-model="dialog" width="500" persistent>
+        <v-card>
+          <v-card-title> Preview of Beneficiary Details </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
+            <v-btn color="primary" @click="dialog = false"> Confirm </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card>
   </div>
 </template>
@@ -188,6 +212,9 @@ export default {
     requiredRuleVComboBox: (v) => {
       return !!v.length || 'Required'
     },
+
+    dialog: false,
+
     validArrayOfCheckboxes: true,
     currentBeneficiary: null,
     currentBeneficiaryIndex: -1,
@@ -215,6 +242,13 @@ export default {
   },
 
   methods: {
+    confirmAddOrUpdateBeneficiary(toUpdatebeneficiary) {
+      const newBeneficiary = this.getFormData(toUpdatebeneficiary)
+      if (!newBeneficiary) return
+
+      this.dialog = true
+    },
+
     addBeneficiary() {
       const newBeneficiaries = [...this.beneficiaries]
       const newBeneficiary = this.getFormData()

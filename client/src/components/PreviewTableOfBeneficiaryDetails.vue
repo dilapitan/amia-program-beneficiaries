025ca9ea84793a6e354fdc35b1c7ae7f2061cf19
dilapitan <domlapitan@gmail.com>
2022-12-28 05:12:41 +0800

    Preview Part 0

--- client/src/components/PreviewTableOfBeneficiaryDetails.vue
+++ client/src/components/PreviewTableOfBeneficiaryDetails.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <p v-if="loadedData">{{ part0FormData.nameOfInterviewer }}</p>
+    <div v-if="loadedData">
+      <v-data-table
+        :headers="part0FormHeaders"
+        :items="part0FormData"
+        disable-filtering
+        disable-pagination
+        disable-sort
+        hide-default-footer
+        dense
+      ></v-data-table>
+    </div>
     <p v-else>Loading data...</p>
   </div>
 </template>
@@ -11,6 +21,10 @@ export default {
 
   data: () => ({
     part0FormData: null,
+    part0FormHeaders: [
+      { text: 'Label', value: 'label' },
+      { text: 'Value', value: 'value' },
+    ],
   }),
 
   props: ['beneficiaryDetails'],
@@ -37,13 +51,13 @@ export default {
         nameOfInterviewer,
       } = beneficiaryDetails
 
-      this.part0FormData = {
-        surveyNo,
-        date,
-        interviewStart,
-        interviewEnd,
-        nameOfInterviewer,
-      }
+      this.part0FormData = [
+        { label: 'Survey Number', value: surveyNo },
+        { label: 'Date', value: date },
+        { label: 'Interview Start', value: interviewStart },
+        { label: 'Interview End', value: interviewEnd },
+        { label: 'Name of Interviewer', value: nameOfInterviewer },
+      ]
     },
   },
 }

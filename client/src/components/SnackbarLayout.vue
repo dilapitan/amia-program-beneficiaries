a271163d076b0efe093b3d68df08b723d31b7457
dilapitan <domlapitan@gmail.com>
2023-01-01 22:16:48 +0800

    Add Snackbar Layout component

---
+++ client/src/components/SnackbarLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-snackbar :color="snackbarDetails?.color" v-model="snackbar">
+    {{ snackbarDetails?.text }}
+    <template v-slot:action="{ attrs }">
+      <v-btn icon color="white" v-bind="attrs" @click="closeSnackbar()">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: 'SnackbarLayout',
+
+  computed: {
+    snackbar: {
+      get() {
+        return this.$store.state.snackbar
+      },
+
+      set() {},
+    },
+
+    snackbarDetails() {
+      return this.$store.state.snackbarDetails
+    },
+  },
+
+  methods: {
+    closeSnackbar() {
+      this.$store.dispatch('setSnackbarAction', false)
+      this.$store.dispatch('setSnackbarActionDetails', null)
+    },
+  },
+}
+</script>

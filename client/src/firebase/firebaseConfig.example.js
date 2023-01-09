ea80f85d0ef816bb75825757761e52ae55b88274
dilapitan <domlapitan@gmail.com>
2023-01-10 07:15:05 +0800

    Update firebaseConfig.example

--- client/src/firebase/firebaseConfig.example.js
+++ client/src/firebase/firebaseConfig.example.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
+import { getFirestore } from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: 'YOUR_FIREBASE_API_KEY',
   authDomain: 'YOUR_FIREBASE_AUTH_DOMAIN',
@@ -13,5 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 const auth = getAuth()
+const db = getFirestore(app)
 
-export { app, auth }
+export { app, auth, db }

2cbb356af234fbe6d0b4524b0b21b45bd4709c29
dilapitan <domlapitan@gmail.com>
2023-01-16 18:36:26 +0800

    update main.js to add choice for using Firebase or not

--- client/src/main.js
+++ client/src/main.js
@@ -8,16 +8,26 @@ import { auth } from '@/firebase/firebaseConfig'
 
 Vue.config.productionTip = false
 
-let app
-auth.onAuthStateChanged((user) => {
-  store.dispatch('setUserAction', user).then(() => {
-    if (!app) {
-      app = new Vue({
-        router,
-        store,
-        vuetify,
-        render: (h) => h(App),
-      }).$mount('#app')
-    }
+if (+process.env.VUE_APP_USE_FIREBASE) {
+  let app
+  auth.onAuthStateChanged((user) => {
+    store.dispatch('setUserAction', user).then(() => {
+      if (!app) {
+        app = new Vue({
+          router,
+          store,
+          vuetify,
+          render: (h) => h(App),
+        }).$mount('#app')
+      }
+    })
   })
-})
+} else {
+  console.log('Using dummy data for ease of development.')
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount('#app')
+}

52d3046a798c5fa364732724e2669c853b6b8c6e
dilapitan <domlapitan@gmail.com>
2023-01-17 20:06:48 +0800

    Add page for Under Construction

---
+++ client/src/components/UnderConstruction.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <div class="text-center">
+      <br /><br />
+      <strong>AMIA Baseline Survey is under construction. </strong>
+      <br />
+      Please contact the admin for more info.
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UnderConstruction',
+}
+</script>

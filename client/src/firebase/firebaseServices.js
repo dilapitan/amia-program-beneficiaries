a2f62684bd6bdb927995e46e2862db5057024f57
dilapitan <domlapitan@gmail.com>
2023-01-26 23:27:25 +0800

    Update Edit Beneficiary call

--- client/src/firebase/firebaseServices.js
+++ client/src/firebase/firebaseServices.js
@@ -1,5 +1,12 @@
 import { db } from '@/firebase/firebaseConfig'
-import { addDoc, collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from 'firebase/firestore'
 
 export const getBeneficiaries = async () => {
   try {
@@ -39,7 +46,39 @@ export const addBeneficiary = async (beneficiary) => {
   }
 }
 
-export const editBeneficiary = async () => {}
+export const editBeneficiary = async (beneficiary) => {
+  console.log('beneficiary:', beneficiary)
+
+  try {
+    const beneficiaryRef = doc(db, 'beneficiaries', beneficiary.beneficiaryId)
+    const docRef = await updateDoc(beneficiaryRef, {
+      createdAt: beneficiary.createdAt,
+      part0: {
+        date: beneficiary.part0.date,
+        interviewStart: beneficiary.part0.interviewStart,
+        interviewEnd: beneficiary.part0.interviewEnd,
+        nameOfInterviewer: beneficiary.part0.nameOfInterviewer,
+        surveyNo: beneficiary.part0.surveyNo,
+      },
+      part1: {
+        barangay: beneficiary.part1.barangay,
+        cityOrMunicipality: beneficiary.part1.cityOrMunicipality,
+        contactNo: beneficiary.part1.contactNo,
+        farmersCodeNo: beneficiary.part1.farmersCodeNo,
+        nameOfFarmer: beneficiary.part1.nameOfFarmer,
+        province: beneficiary.part1.province,
+      },
+      userId: beneficiary.userId,
+    })
+    if (docRef) {
+      console.log(docRef)
+      return docRef
+    }
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
 
 export const deleteBeneficiary = async (beneficiary) => {
   try {

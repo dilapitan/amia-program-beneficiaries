fa6cba60ee4044c3802bc6086d8550de9682f1d5
dilapitan <domlapitan@gmail.com>
2023-02-17 23:05:17 +0800

    Use service worker file

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 import { auth } from '@/firebase/firebaseConfig'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
